c0982e3a6f13f52512e6680478089f01f76fdf4d
VeronikaHh <golovanovaveronika@gmail.com>
2024-04-14 12:59:36 +0300

    Added typings, updated README.MD, added requirements.txt

--- balance_change.py
+++ balance_change.py
@@ -1,8 +1,8 @@
 class BalanceChange:
-    def __init__(self, user_id, value, currency):
+    def __init__(self, user_id, value, currency) -> None:
         self.user_id = user_id
         self.value = value
         self.currency = currency
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'BalanceChange(user_id={self.user_id}, value={self.value}, currency={self.currency})'
